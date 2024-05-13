eb771b272412e2fa2ee53cc70728b70cbd5d80d2
mellsvs <meltemsavas192@gmail.com>
2024-05-13 16:11:06 +0300

    created home page

--- hotel-full-stack/hotel-frontend/Lakeside-hotel/src/components/home/Home.jsx
+++ hotel-full-stack/hotel-frontend/Lakeside-hotel/src/components/home/Home.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useContext } from "react"
+import MainHeader from "../layout/MainHeader"
+import HotelService from "../common/HotelService"
+import Parallax from "../common/Parallax"
+
 
 const Home = () => {
-  return (
-    <div>
-        <h2>
-            WELCOME TO HOME PAGE
-        </h2>
-    </div>
-  )
+	
+	return (
+		<section>
+     <MainHeader />
+			
+      <section className="container">
+
+      <Parallax/>
+      <HotelService/>
+      <Parallax/>
+
+      </section>
+		</section>
+	)
 }
 
 export default Home
