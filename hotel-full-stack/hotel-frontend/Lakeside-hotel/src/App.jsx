bb1e436664bd8ad4282550f6a163c304278dfd1d
mellsvs <meltemsavas192@gmail.com>
2024-05-13 15:24:01 +0300

    listing all room

--- hotel-full-stack/hotel-frontend/Lakeside-hotel/src/App.jsx
+++ hotel-full-stack/hotel-frontend/Lakeside-hotel/src/App.jsx
@@ -8,6 +8,7 @@ import EditRoom from "./components/room/EditRoom"
 import AddRoom from "./components/room/AddRoom"
 import NavBar from "./components/layout/NavBar"
 import Footer from "./components/layout/Footer"
+import RoomListing from "./components/room/RoomListing"
 
 function App() {
   
@@ -21,8 +22,8 @@ function App() {
 						<Route path="/edit-room/:roomId" element={<EditRoom />} />
 						<Route path="/existing-rooms" element={<ExistingRooms />} />
 						<Route path="/add-room" element={<AddRoom />} />
+            <Route path="/browse-all-rooms" element={<RoomListing />} />
           </Routes>
-
       </Router>
       <Footer />
     </main>
